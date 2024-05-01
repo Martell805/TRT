c4897c8e6ae05045cf6dd6a3d8faba641bd761d5
Martell <vovasamb@yandex.ru>
2024-05-01 18:15:47 +0300

    PR5

---
+++ pr5/update.sql
@@ -0,0 +1,11 @@
+INSERT INTO customer (name, address, city, state, zip_code, area_code, phone_number, salesperson_id, credit_limit, comments)
+VALUES ('Новый Партнёр', 'ул. Партнёра, д.1', 'Москва', 'МО', '111111', 111, 11111, 5, 100000.00,
+        'Самый новый клиент');
+
+INSERT INTO customer (name, address, city, state, zip_code, area_code, phone_number, salesperson_id, credit_limit, comments)
+VALUES ('1422351253 Партнёр', 'ул. Партнёра, д.2', 'Москва', 'МО', '111111', 111, 11111, 5, 100000.00,
+        'XXXXXXXX');
+
+INSERT INTO customer (name, address, city, state, zip_code, area_code, phone_number, salesperson_id, credit_limit, comments)
+VALUES ('-1 Партнёр', 'ул. Партнёра, д.3', 'Москва', 'МО', '111111', 111, 11111, 5, 100000.00,
+        'VVVVVVVVVVV');
